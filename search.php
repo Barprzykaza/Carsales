11faa40d8f7c81459eaaf5528d3151d24cfdaff0
Barprzykaza <bartek.przykaza123@gmail.com>
2025-03-26 19:02:15 +0100

    Update search.php

--- search.php
+++ search.php
@@ -9,7 +9,6 @@
     <script src="https://kit.fontawesome.com/baf9ff804b.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
-    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" rel="stylesheet" />
 
     <link rel="stylesheet" href="css/style.css">
